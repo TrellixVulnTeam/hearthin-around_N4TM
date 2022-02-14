145c7a3cc3caf7f7c276b267dc764b3706a6a9fe
Blake Dykes <56227216+BlakeDykes@users.noreply.github.com>
2022-02-14 15:22:24 -0600

    restructured client

--- server/index.js
+++ server/index.js
@@ -1,18 +1,20 @@
 const express = require("express");
+const axios = require('axios');
 const path = require('path');
-
-const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '/client/build')));
+const PORT = process.env.PORT || 5000;
+
+
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
 
 app.get("/api", (req, res) => {
     res.json({message: "Hello from server!"});
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
 })
 
 app.listen(PORT, () => {
